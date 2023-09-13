d3745806475755e5df2f45f08eddc55468341550
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2023-09-13 17:04:00 +0530

    Updated
    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgetPasswordComponent } from './components/auth/forget-password/forget-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ForgetPasswordComponent
   ],
   imports: [
     BrowserModule,
