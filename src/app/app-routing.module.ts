3f7827da5106f104ab1200442ed4a8a8ce9564e4
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2023-09-16 00:41:12 +0530

    Updated
    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component'; 
+import { ForgetPasswordComponent } from './components/auth/forget-password/forget-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }, 
+  {path: 'password', component: ForgetPasswordComponent }, 
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
