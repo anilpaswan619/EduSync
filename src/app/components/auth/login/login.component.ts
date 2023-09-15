bb56ee0bc12eef5cf22fd305f7de3de87b6dd9e7
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2023-09-15 10:50:28 +0530

    Updated
    Updated

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -9,10 +9,34 @@ export class LoginComponent {
   showButtons = false;
   buttonClicked = false;
   showParentForm = false;
+  isParentClicked = false;
+  isStudentClicked = false;
+  isTeacherClicked = false;
 
   toggleParentForm() {
     this.showParentForm = true;
     this.buttonClicked = true;
     this.showButtons = false;
+    this.isParentClicked = true;
+    this.isStudentClicked = false;
+    this.isTeacherClicked = false;
   }
+  toggleStudentForm() {
+    this.showParentForm = true;
+    this.buttonClicked = true;
+    this.showButtons = false;
+    this.isParentClicked = false;
+    this.isStudentClicked = true;
+    this.isTeacherClicked = false;
+  }
+
+  toggleTeacherForm() {
+    this.showParentForm = true;
+    this.buttonClicked = true;
+    this.showButtons = false;
+    this.isParentClicked = false;
+    this.isStudentClicked = false;
+    this.isTeacherClicked = true;
+  }
+
 }
