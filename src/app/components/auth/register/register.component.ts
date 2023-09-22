ce45f7e51053458cda8c49a8b0a1223c4416fb9c
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2023-09-22 19:06:42 +0530

    Updated

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -27,18 +27,24 @@ export class RegisterComponent {
     emailCtrl: ['', [Validators.required, Validators.email]],
     passwordCtrl: ['', Validators.required],
     confirmPasswordCtrl: ['', Validators.required],
+    familyIDCtrl: ['', Validators.required],
+    occupationCtrl: ['', Validators.required],
   });
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
     childNameCtrl: ['', Validators.required],
     classCtrl: ['', Validators.required],
     ageCtrl: ['', Validators.required],
+    itsNumberCtrl: ['', Validators.required],
+    gradeCtrl: ['', Validators.required],
   });
   thirdFormGroup = this._formBuilder.group({
     thirdCtrl: ['', Validators.required],
     cityCtrl: ['', Validators.required],
     stateCtrl: ['', Validators.required],
     countryCtrl: ['', Validators.required],
+    phoneCtrl: ['', Validators.required],
+    workEmailCtrl: ['', [Validators.required, Validators.email]],
   });
   stepperOrientation: Observable<StepperOrientation>;
 
